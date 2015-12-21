f52c287b6779c41f9150c9e6439c4c1291838897
Justin LeFurjah <lefurjah@gmail.com>
2015-12-21 13:27:47 -0600

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221171035) do
+ActiveRecord::Schema.define(version: 20151221192527) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
