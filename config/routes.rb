688ca8b24fcbed2620b6940eb4b4306c3815749c
Justin LeFurjah <lefurjah@gmail.com>
2015-12-21 09:13:59 -0600

    nav routes up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+  # get 'login' => '#'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
