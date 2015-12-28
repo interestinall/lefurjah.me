182eabf740b663d4cb5648edc00ac945cd5f832f
Justin LeFurjah <lefurjah@gmail.com>
2015-12-28 13:14:10 -0600

    deleted log in, created 'backdoor'- log out

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,5 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
+  get 'backdoor' => 'sessions#new'
+
   get 'login' => 'sessions#new'
   post'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
