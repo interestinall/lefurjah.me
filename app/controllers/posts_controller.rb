eec0a83073750732080331d14bc3e0c8c9f4f858
Justin LeFurjah <lefurjah@gmail.com>
2016-01-04 12:25:22 -0600

    reverse_each

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all(:order => "created_at DESC")
+    @posts = Post.all
   end
 
   # GET /posts/1
