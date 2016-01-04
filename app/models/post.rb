b61a5b929f4bc90224049740786f6c394448caf3
Justin LeFurjah <lefurjah@gmail.com>
2016-01-04 12:38:09 -0600

    one more time

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
-
+  
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
